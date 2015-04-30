f607ae8394bf3101037672fb3c19a0511f01d98e
Mark Smith <zorkian@dropbox.com>
2015-04-30 14:15:12 -0700

    Fix infinite loop regex
    There was a bug in the regex that could allow accidental denials of
    service when certain permissions were input.

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 
 import grouper.fe.util
@@ -5,7 +6,7 @@ import grouper.fe.util
 from fixtures import standard_graph, graph, users, groups, session, permissions  # noqa
 from util import get_group_permissions, get_user_permissions
 
-from grouper.constants import PERMISSION_AUDITOR
+from grouper.constants import PERMISSION_AUDITOR, PERMISSION_VALIDATION
 
 
 def test_basic_permission(standard_graph, session, users, groups, permissions):  # noqa
@@ -28,9 +29,20 @@ def test_basic_permission(standard_graph, session, users, groups, permissions): 
         "audited:", PERMISSION_AUDITOR + ":", "ssh:*", "sudo:shell"]
     assert sorted(get_user_permissions(graph, "testuser")) == []
 
+
 class PermissionTests(unittest.TestCase):
     def test_reject_bad_permission_names(self):
         self.assertEquals(len(grouper.fe.util.test_reserved_names("permission_lacks_period")), 1)
         self.assertEquals(len(grouper.fe.util.test_reserved_names("grouper.prefix.reserved")), 1)
         self.assertEquals(len(grouper.fe.util.test_reserved_names("admin.prefix.reserved")), 1)
         self.assertEquals(len(grouper.fe.util.test_reserved_names("test.prefix.reserved")), 1)
+
+        def eval_permission(perm):
+            return re.match(r'^' + PERMISSION_VALIDATION + r'$', perm)
+
+        self.assertIsNotNone(eval_permission('foo.bar'))
+        self.assertIsNotNone(eval_permission('foobar'))
+        self.assertIsNotNone(eval_permission('foo.bar_baz'))
+        self.assertIsNone(eval_permission('foo__bar'))
+        self.assertIsNone(eval_permission('foo.bar.'))
+        self.assertIsNone(eval_permission('foo._bar'))

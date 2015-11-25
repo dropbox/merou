6bdcf3fb9236217ed756db215d4245e1ebcea665
Luke Faraone <lfaraone@dropbox.com>
2015-11-25 01:26:56 +0000

    Create a grouper-administrators group in sync_db
    This group is populated with all SYSTEM_PERMISSIONS

--- grouper/ctl/sync_db.py
+++ grouper/ctl/sync_db.py
@@ -4,6 +4,7 @@ from grouper.constants import SYSTEM_PERMISSIONS
 from grouper.ctl.util import make_session
 from grouper.models import (
         get_db_engine,
+        Group,
         Model,
         Permission,
         )
@@ -17,10 +18,36 @@ def sync_db_command(args):
 
     # Add some basic database structures we know we will need if they don't exist.
     session = make_session()
+    # This group is needed to bootstrap a Grouper installation.
+    admin_group = Group.get(session, name="grouper-administrators")
+    if not admin_group:
+        admin_group = Group(
+                groupname="grouper-administrators",
+                description="Administrators of the Grouper system.",
+                canjoin="nobody",
+        )
+
+        try:
+            admin_group.add(session)
+            session.flush()
+        except IntegrityError:
+            session.rollback()
+            raise Exception('Failed to create group: grouper-administrators')
+    admin_group_permission_names = [perm[1] for perm in admin_group.my_permissions()]
+
     for name, description in SYSTEM_PERMISSIONS:
-        test = Permission.get(session, name)
-        if test:
+        permission = Permission.get(session, name)
+        if permission and name not in admin_group_permission_names:
+            try:
+                admin_group.grant_permission(permission)
+                session.commit()
+            except IntegrityError:
+                # The permission is already extant in the group, carry on.
+                session.rollback()
             continue
+        elif permission:
+            continue
+
         permission = Permission(name=name, description=description)
         try:
             permission.add(session)
@@ -29,6 +56,10 @@ def sync_db_command(args):
             session.rollback()
             raise Exception('Failed to create permission: %s' % (name, ))
         session.commit()
+        admin_group.grant_permission(permission)
+        session.commit()
+
+    session.commit()
 
 
 def add_parser(subparsers):

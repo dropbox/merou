2b8ec0f50284b90080342b89552814ca9b514bf7
Herbert Ho <herb@dropbox.com>
2016-01-27 13:54:56 -0800

    parameterize user_proxy listen host

--- grouper/ctl/user_proxy.py
+++ grouper/ctl/user_proxy.py
@@ -17,13 +17,13 @@ def user_proxy_command(args):
         logging.debug("No username provided, using (%s)", username)
 
     server = BaseHTTPServer.HTTPServer(
-        ('localhost', args.listen_port), UserProxyHandler
+        (args.listen_host, args.listen_port), UserProxyHandler
     )
     server.args = ServerArgs(args.backend_port, args.username)
     try:
         logging.info(
-            "Starting user_proxy on port (%s) with user (%s)",
-            args.listen_port, username
+            "Starting user_proxy on host (%s) and port (%s) with user (%s)",
+            args.listen_host, args.listen_port, username
         )
         server.serve_forever()
     except KeyboardInterrupt:
@@ -34,6 +34,8 @@ def add_parser(subparsers):
     user_proxy_parser = subparsers.add_parser("user_proxy",
                                               help="Start a development reverse proxy.")
     user_proxy_parser.set_defaults(func=user_proxy_command)
+    user_proxy_parser.add_argument("--listen-host", default="localhost",
+                                   help="Host to listen on.")
     user_proxy_parser.add_argument("-p", "--listen-port", default=8888, type=int,
                                    help="Port to listen on.")
     user_proxy_parser.add_argument("-P", "--backend-port", default=8989, type=int,

34e4c7d60b547eecb2a5767e2164e12d5dd5a634
Luke Faraone <lfaraone@dropbox.com>
2016-02-22 20:45:24 +0000

    Store secret hash, rather than secret

--- grouper/models.py
+++ grouper/models.py
@@ -1,4 +1,5 @@
 import functools
+import hashlib
 import hmac
 import json
 import logging
@@ -652,7 +653,9 @@ class UserToken(Model):
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     disabled_at = Column(DateTime, default=None, nullable=True)
 
-    secret = Column(String(length=32), default=_make_secret, unique=True, nullable=False)
+    hashed_secret = Column('secret', String(length=64), unique=True, nullable=False)
+
+    secret = None
 
     user = relationship("User", back_populates="tokens")
 
@@ -680,14 +683,23 @@ class UserToken(Model):
             return session.query(UserToken).filter_by(name=name, user=user).scalar()
         return session.query(UserToken).filter_by(id=id, user=user).scalar()
 
+    def _set_secret(self):
+        self.secret = _make_secret()
+        self.hashed_secret = hashlib.sha256(self.secret).hexdigest()
+
     def add(self, session):
+        if self.secret is None:
+            self._set_secret()
         super(UserToken, self).add(session)
         Counter.incr(session, "updates")
         return self
 
     def check_secret(self, secret):
-        # The length of self.secret is not secret
-        return self.enabled and hmac.compare_digest(secret, self.secret)
+        # The length of self.hashed_secret is not secret
+        return self.enabled and hmac.compare_digest(
+                hashlib.sha256(secret).hexdigest(),
+                self.hashed_secret.encode('utf-8'),
+        )
 
     @property
     def enabled(self):

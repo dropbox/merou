a62896db7e8d8559bdefd210a5b5e05f1cb613e8
Tyler O'Meara <tyleromeara@dropbox.com>
2016-06-08 13:16:58 -0700

    Change the default value for the shell selection to /bin/false
    Signed-off-by: Tyler O'Meara <tyleromeara@dropbox.com>

--- tests/test_shells.py
+++ tests/test_shells.py
@@ -1,53 +1,57 @@
-import json
 from urllib import urlencode
-from mock import patch
 
+from mock import patch
 import pytest
-from tornado.httpclient import HTTPError
 
 from fixtures import standard_graph, graph, users, groups, session, permissions  # noqa
 from fixtures import fe_app as app  # noqa
-from grouper import public_key
-from grouper.model_soup import User, Request, AsyncNotification
-from grouper.constants import USER_METADATA_SHELL_KEY
 from url_util import url
+
+from grouper.constants import USER_METADATA_SHELL_KEY
+from grouper.model_soup import User
 
 
 @pytest.mark.gen_test
 def test_shell(session, users, http_client, base_url):
-    user = users['zorkian@a.co']
-    assert not user.get_metadata(USER_METADATA_SHELL_KEY)
+    with patch('grouper.fe.handlers.user_shell.settings') as mock_settings:
+        mock_settings.shell = [['/bin/bash', 'bash'], ['/bin/zsh', 'zsh']]
 
-    user = User.get(session, name=user.username)
-    fe_url = url(base_url, '/users/{}/shell'.format(user.username))
-    resp = yield http_client.fetch(fe_url, method="POST",
-            body=urlencode({'shell': "/bin/bash"}),
-            headers={'X-Grouper-User': user.username})
-    assert resp.code == 200
+        user = users['zorkian@a.co']
+        assert not user.get_metadata(USER_METADATA_SHELL_KEY)
 
-    user = User.get(session, name=user.username)
+        user = User.get(session, name=user.username)
+        fe_url = url(base_url, '/users/{}/shell'.format(user.username))
+        resp = yield http_client.fetch(fe_url, method="POST",
+                body=urlencode({'shell': "/bin/bash"}),
+                headers={'X-Grouper-User': user.username})
+        assert resp.code == 200
 
-    assert user.get_metadata(USER_METADATA_SHELL_KEY) is not None, "The user should have shell metadata"
-    assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/bash"
+        user = User.get(session, name=user.username)
 
-    fe_url = url(base_url, '/users/{}/shell'.format(user.username))
-    resp = yield http_client.fetch(fe_url, method="POST",
-            body=urlencode({'shell': "/bin/fish"}),
-            headers={'X-Grouper-User': user.username})
-    assert resp.code == 200
+        assert (user.get_metadata(USER_METADATA_SHELL_KEY) is not None,
+            "The user should have shell metadata")
+        assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/bash"
 
-    user = User.get(session, name=user.username)
+        fe_url = url(base_url, '/users/{}/shell'.format(user.username))
+        resp = yield http_client.fetch(fe_url, method="POST",
+                body=urlencode({'shell': "/bin/fish"}),
+                headers={'X-Grouper-User': user.username})
+        assert resp.code == 200
 
-    assert user.get_metadata(USER_METADATA_SHELL_KEY) is not None, "The user should have shell metadata"
-    assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/bash"
+        user = User.get(session, name=user.username)
 
-    fe_url = url(base_url, '/users/{}/shell'.format(user.username))
-    resp = yield http_client.fetch(fe_url, method="POST",
-            body=urlencode({'shell': "/bin/zsh"}),
-            headers={'X-Grouper-User': user.username})
-    assert resp.code == 200
+        assert (user.get_metadata(USER_METADATA_SHELL_KEY) is not None,
+            "The user should have shell metadata")
+        assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/bash"
 
-    user = User.get(session, name=user.username)
+        fe_url = url(base_url, '/users/{}/shell'.format(user.username))
+        resp = yield http_client.fetch(fe_url, method="POST",
+                body=urlencode({'shell': "/bin/zsh"}),
+                headers={'X-Grouper-User': user.username})
+        assert resp.code == 200
 
-    assert user.get_metadata(USER_METADATA_SHELL_KEY) is not None, "The user should have shell metadata"
-    assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/zsh"
+        user = User.get(session, name=user.username)
+
+        assert (user.get_metadata(USER_METADATA_SHELL_KEY) is not None,
+            "The user should have shell metadata")
+        assert user.get_metadata(USER_METADATA_SHELL_KEY).data_value == "/bin/zsh"

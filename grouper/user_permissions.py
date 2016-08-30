dropbox/merou

commit 3418473f6de868403f1bbcedebe2139fc5de4b07

user_permissions now correctly returns all user permissions
It pulls all permissions, including those inherited from groups
from which the user is not a direct member.

grouper/user_permissions.py
@@ -4,9 +4,10 @@ from sqlalchemy import asc, or_
 
 from grouper.constants import (GROUP_ADMIN, PERMISSION_ADMIN, PERMISSION_CREATE,
     PERMISSION_GRANT, USER_ADMIN)
-from grouper.model_soup import Group, GroupEdge
+from grouper.model_soup import Group, GROUP_EDGE_ROLES, GroupEdge
 from grouper.models.permission import Permission
 from grouper.models.permission_map import PermissionMap
+from grouper.user_group import get_all_groups_by_user
 
 
 def user_has_permission(session, user, permission, argument=None):
@@ -35,7 +36,9 @@ def user_has_permission(session, user, permission, argument=None):
 
 def user_permissions(session, user):
 
-    # TODO: Make this walk the tree, so we can get a user's entire set of permissions.
+    user_groups = get_all_groups_by_user(session, user)
+    non_np = [grp.name for grp, role in user_groups if role != GROUP_EDGE_ROLES.index("np-owner")]
+
     now = datetime.utcnow()
     permissions = session.query(
         Permission.name,
@@ -46,11 +49,10 @@ def user_permissions(session, user):
         PermissionMap.permission_id == Permission.id,
         PermissionMap.group_id == Group.id,
         GroupEdge.group_id == Group.id,
-        GroupEdge.member_pk == user.id,
-        GroupEdge.member_type == 0,
         GroupEdge.active == True,
         user.enabled == True,
         Group.enabled == True,
+        Group.name.in_(non_np),
         or_(
             GroupEdge.expiration > now,
             GroupEdge.expiration == None

grouper/user_group.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 from sqlalchemy import or_
+from sqlalchemy.orm import Session  # noqa
 
 from grouper.model_soup import Group, GroupEdge, OWNER_ROLE_INDICES
+from grouper.models.user import User  # noqa
 
 
 def get_groups_by_user(session, user):
@@ -21,6 +23,22 @@ def get_groups_by_user(session, user):
             Group.enabled == True,
             or_(GroupEdge.expiration > now, GroupEdge.expiration == None),
             ).all()
+
+
+def get_all_groups_by_user(session, user):
+    # type: (Session, User) -> List[tuple[Group, int]]
+    """Return groups a given user is a member of along with the user's role.
+
+    This includes groups inherited from other groups, unlike get_groups_by_user.
+
+    Args:
+        session(): database session
+        user(models.User): model for user in question
+    """
+    from grouper.graph import Graph
+    grps = Graph().get_user_details(username=user.name)["groups"]
+    groups = session.query(Group).filter(Group.name.in_(grps.keys())).all()
+    return [(group, grps[group.name]["role"]) for group in groups]
 
 
 def user_can_manage_group(session, group, user):

commit aa425a9d4cd78fa64ae2d90111288430aa098a86

Partially revert 3418473f6d due to perf regression
3418473f6d introduced a perf regression in the /users/:email endpoint due to an
inefficient query, raising average TTI in production from ~2s to ~30s.

grouper/user_permissions.py
@@ -4,10 +4,9 @@ from sqlalchemy import asc, or_
 
 from grouper.constants import (GROUP_ADMIN, PERMISSION_ADMIN, PERMISSION_CREATE,
     PERMISSION_GRANT, USER_ADMIN)
-from grouper.model_soup import Group, GROUP_EDGE_ROLES, GroupEdge
+from grouper.model_soup import Group, GroupEdge
 from grouper.models.permission import Permission
 from grouper.models.permission_map import PermissionMap
-from grouper.user_group import get_all_groups_by_user
 
 
 def user_has_permission(session, user, permission, argument=None):
@@ -36,9 +35,7 @@ def user_has_permission(session, user, permission, argument=None):
 
 def user_permissions(session, user):
 
-    user_groups = get_all_groups_by_user(session, user)
-    non_np = [grp.name for grp, role in user_groups if role != GROUP_EDGE_ROLES.index("np-owner")]
-
+    # TODO: Make this walk the tree, so we can get a user's entire set of permissions.
     now = datetime.utcnow()
     permissions = session.query(
         Permission.name,
@@ -49,10 +46,11 @@ def user_permissions(session, user):
         PermissionMap.permission_id == Permission.id,
         PermissionMap.group_id == Group.id,
         GroupEdge.group_id == Group.id,
+        GroupEdge.member_pk == user.id,
+        GroupEdge.member_type == 0,
         GroupEdge.active == True,
         user.enabled == True,
         Group.enabled == True,
-        Group.name.in_(non_np),
         or_(
             GroupEdge.expiration > now,
             GroupEdge.expiration == None
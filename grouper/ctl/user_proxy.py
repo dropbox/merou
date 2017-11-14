549f1f567f681f3b973cb6e58917feb94d92b643
Can Berk Güder <cbguder@dropbox.com>
2017-11-13 16:11:04 -0800

    Fix user proxy's handling of multiple Set-Cookie headers
    This is a workaround for https://github.com/gmjosack/mrproxy/issues/2

--- grouper/ctl/user_proxy.py
+++ grouper/ctl/user_proxy.py
@@ -1,25 +1,60 @@
 import BaseHTTPServer
 import getpass
 import logging
+import urllib2
 
 from mrproxy import UserProxyHandler
 
 
-def user_proxy_command(args):
+# Workaround for https://github.com/gmjosack/mrproxy/issues/2
+class ProxyHandler(UserProxyHandler):
+    def do_request(self, request):
+        try:
+            url = urllib2.urlopen(request)
+            code = url.getcode()
+            headers = str(url.info())
+            data = url.read()
+        except urllib2.HTTPError as err:
+            code = err.getcode()
+            headers = str(err.info())
+            data = err.read()
+        except urllib2.URLError as err:
+            code = 503
+            headers = str(err.info())
+            data = "503 Service Unavailable: %s\n" % err
+
+        self.send_response(code)
+        self.wfile.write(headers)
+        self.end_headers()
+        self.wfile.write(data)
+
+
+def build_user_proxy_server(username, backend_port, listen_host, listen_port):
     class ServerArgs(object):
         def __init__(self, backend_port, username):
             self.backend_port = backend_port
             self.header = ["X-Grouper-User: %s" % username]
 
+    server = BaseHTTPServer.HTTPServer(
+        (listen_host, listen_port), ProxyHandler
+    )
+    server.args = ServerArgs(backend_port, username)
+
+    return server
+
+
+def user_proxy_command(args):
     username = args.username
     if username is None:
         username = getpass.getuser()
         logging.debug("No username provided, using (%s)", username)
 
-    server = BaseHTTPServer.HTTPServer(
-        (args.listen_host, args.listen_port), UserProxyHandler
+    server = build_user_proxy_server(
+        args.username,
+        args.backend_port,
+        args.listen_host,
+        args.listen_port
     )
-    server.args = ServerArgs(args.backend_port, args.username)
     try:
         logging.info(
             "Starting user_proxy on host (%s) and port (%s) with user (%s)",

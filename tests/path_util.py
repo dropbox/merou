21a539f37ec945371f6a5e4d8c59b5ba515362e8
Can Berk Güder <cbguder@dropbox.com>
2017-11-16 17:18:44 -0800

    Added TODO to remove src_path

--- tests/path_util.py
+++ tests/path_util.py
@@ -1,6 +1,7 @@
 import os
 
 
+# TODO(cbguder): Replace uses of this function with something that doesn't depend on code layout
 def src_path(*args):
     root = os.path.join(__file__, "..", "..")
     path = os.path.join(root, *args)

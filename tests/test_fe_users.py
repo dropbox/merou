dropbox/merou

commit d005a71947a450e8fe26cabfc0e78fe4b68b8ad5

Template cleanup
Move duplicated public key macros to macros/ui.html, fix mismatched
closing tags.

grouper/fe/templates/macros/ui.html
@@ -39,6 +39,90 @@
     </div>
 {%- endmacro %}
 
+{% macro one_public_key_panel_row(key, username, can_control) -%}
+    <td><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
+        {{ key.created_on|long_ago_str }}</span></td>
+    <td>{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
+    <td>{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
+    <td class="hidden-xs">
+        <code>{{ key.fingerprint }}</code>
+    </td>
+    <td>
+        {% for tag in key.tags %}
+            {% if can_control %}
+            <form action="/users/{{ username }}/public-key/{{ key.id }}/delete_tag/{{ tag.id }}" method="post" style="display: inline">
+            {% endif %}
+                <button type="submit" class="btn btn-danger">
+                    {{ tag.name }}
+                </button>
+            {% if can_control %}
+                {{ xsrf_form() }}
+            </form>
+            {% endif %}
+        {% endfor %}
+    </td>
+    <td>
+        <span class="pull-right">
+        <button class="btn btn-default btn-xs public-key" key-body="{{ key.public_key|escape }}">
+            <i class="fa fa-search"></i>
+        </button>
+        <button class="btn btn-default btn-xs public-key" key-body="{{ ",".join(key.pretty_permissions)|escape }}">
+            <i class="fa fa-key"></i>
+        </button>
+    {% if can_control %}
+        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/tag">
+            <span class="glyphicon glyphicon-tag" style="vertical-align: -1px"></span>
+        </a>
+        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/delete">
+            <span class="glyphicon glyphicon-remove" style="vertical-align: -1px"></span>
+        </a>
+        </span>
+    {% endif %}
+    </td>
+{%- endmacro %}
+
+{% macro public_key_panel(max_height, user, public_keys, can_control=False) -%}
+    {{ public_key_modal() }}
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Public Keys</h3>
+        </div>
+        <div class="table-responsive" style="max-height: {{max_height}}px; overflow-y: auto">
+            <table class="table table-striped table-condensed">
+                <thead>
+                    <tr>
+                        <th class="col-sm-1">Age</th>
+                        <th class="col-sm-1">Type</th>
+                        <th class="col-sm-1">Size</th>
+                        <th class="col-sm-3 hidden-xs">Fingerprint</th>
+                        <th class="col-sm-1">Tags</th>
+                        <th class="col-sm-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {% for key in public_keys %}
+                    <tr>
+                        {{ one_public_key_panel_row(key, user.name, can_control) }}
+                    </tr>
+                {% endfor %}
+                {% if not public_keys %}
+                    <tr>
+                        <td colspan="9" class="text-center"><em>No Public Keys</em></td>
+                    </tr>
+                {% endif %}
+                </tbody>
+            </table>
+            {% if can_control %}
+            <div class='panel-footer'>
+                <a class="btn btn-default btn-sm" href="/users/{{ user.name }}/public-key/add">
+                <span class="glyphicon glyphicon-plus"></span> Add Public Key
+                </a>
+            </div>
+            {% endif %}
+        </div>
+    </div>
+{%- endmacro %}
+
 {% macro one_user_token_row(token, username, can_control) -%}
     <td>{{ token.name }}</td>
     <td>{{ token.created_at | long_ago_str }}</td>

grouper/fe/templates/users-publickey.html
@@ -1,5 +1,8 @@
 {% extends "base.html" %}
-{% from 'macros/ui.html' import account, paginator, public_key_modal, one_public_key_row, dropdown with context %}
+
+{% from 'macros/ui.html' import account, paginator, one_public_key_row %}
+{% from 'macros/ui.html' import public_key_modal with context %}
+{% from 'macros/ui.html' import dropdown with context %}
 
 {% block heading %}
     {% if not form.enabled.data %}

grouper/fe/templates/group.html
@@ -9,7 +9,7 @@
 {% block subheading %}
     {{group.groupname}} {% if not group.enabled %}<small>(disabled)</small>{% endif %}
     {% if group.audit and not group.audit.complete %}
-        <span class="label label-danger">AUDIT IN PROGRESS</i>
+        <span class="label label-danger">AUDIT IN PROGRESS</span>
     {% endif %}
     {% if group.email_address %}
         <a href="mailto:{{ group.email_address }}">

grouper/fe/templates/service-account.html
@@ -1,6 +1,6 @@
 {% extends "base.html" %}
-{% from 'macros/ui.html' import group_panel, log_entry_panel, tokens_panel, public_key_modal,
-                                shell_panel, passwords_panel, permission, %}
+{% from 'macros/ui.html' import log_entry_panel, tokens_panel, shell_panel, passwords_panel,
+                                permission, public_key_panel %}
 
 {% macro permission_panel(max_height, mappings, group, user, can_manage) -%}
     <div class="panel panel-default">
@@ -56,90 +56,6 @@
             </a>
         </div>
         {% endif %}
-    </div>
-{%- endmacro %}
-
-{% macro one_public_key_row(key, username, can_control) -%}
-    <td><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
-        {{ key.created_on|long_ago_str }}</span></td>
-    <td>{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
-    <td>{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
-    <td class="hidden-xs">
-        <code>{{ key.fingerprint }}</code>
-    </td>
-    <td>
-        {% for tag in key.tags %}
-            {% if can_control %}
-            <form action="/users/{{ username }}/public-key/{{ key.id }}/delete_tag/{{ tag.id }}" method="post" style="display: inline">
-            {% endif %}
-                <button type="submit" class="btn btn-danger">
-                    {{ tag.name }}
-                </button>
-            {% if can_control %}
-                {{ xsrf_form() }}
-            </form>
-            {% endif %}
-        {% endfor %}
-    </td>
-    <td>
-        <span class="pull-right">
-        <button class="btn btn-default btn-xs public-key" key-body="{{ key.public_key|escape }}">
-            <i class="fa fa-search"></i>
-        </button>
-        <button class="btn btn-default btn-xs public-key" key-body="{{ ",".join(key.pretty_permissions)|escape }}">
-            <i class="fa fa-key"></i>
-        </button>
-    {% if can_control %}
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/tag">
-            <span class="glyphicon glyphicon-tag" style="vertical-align: -1px"></span>
-        </a>
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/delete">
-            <span class="glyphicon glyphicon-remove" style="vertical-align: -1px"></span>
-        </a>
-        </span>
-    {% endif %}
-    </td>
-{%- endmacro %}
-
-{% macro public_key_panel(max_height, user, public_keys, can_control=False) -%}
-    {{ public_key_modal() }}
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Public Keys</h3>
-        </div>
-        <div class="table-responsive" style="max-height: {{max_height}}px; overflow-y: auto">
-            <table class="table table-striped table-condensed">
-                <thead>
-                    <tr>
-                        <th class="col-sm-1">Age</th>
-                        <th class="col-sm-1">Type</th>
-                        <th class="col-sm-1">Size</th>
-                        <th class="col-sm-3 hidden-xs">Fingerprint</th>
-                        <th class="col-sm-1">Tags</th>
-                        <th class="col-sm-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                {% for key in public_keys %}
-                    <tr>
-                        {{ one_public_key_row(key, user.name, can_control) }}
-                    </tr>
-                {% endfor %}
-                {% if not public_keys %}
-                    <tr>
-                        <td colspan="9" class="text-center"><em>No Public Keys</em></td>
-                    </tr>
-                {% endif %}
-                </tbody>
-            </table>
-            {% if can_control %}
-            <div class='panel-footer'>
-                <a class="btn btn-default btn-sm" href="/users/{{ user.username }}/public-key/add">
-                <span class="glyphicon glyphicon-plus"></span> Add Public Key
-                </a>
-            </div>
-            {% endif %}
-        </div>
     </div>
 {%- endmacro %}
 

grouper/fe/templates/service.html
@@ -1,92 +1,10 @@
 {% extends "base.html" %}
+
 {% from 'macros/ui.html' import group_panel, member_panel, permission_panel, log_entry_panel,
-                                help_for, account, shell_panel, tokens_panel, public_key_modal,
-                                passwords_panel, %}
+                                help_for, account, shell_panel, tokens_panel, passwords_panel,
+                                public_key_panel %}
 
-{% macro one_public_key_row(key, username, can_control) -%}
-    <td><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
-        {{ key.created_on|long_ago_str }}</span></td>
-    <td>{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
-    <td>{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
-    <td class="hidden-xs">
-        <code>{{ key.fingerprint }}</code>
-    </td>
-    <td>
-        {% for tag in key.tags %}
-            {% if can_control %}
-            <form action="/users/{{ username }}/public-key/{{ key.id }}/delete_tag/{{ tag.id }}" method="post" style="display: inline">
-            {% endif %}
-                <button type="submit" class="btn btn-danger">
-                    {{ tag.name }}
-                </button>
-            {% if can_control %}
-                {{ xsrf_form() }}
-            </form>
-            {% endif %}
-        {% endfor %}
-    </td>
-    <td>
-        <span class="pull-right">
-        <button class="btn btn-default btn-xs public-key" key-body="{{ key.public_key|escape }}">
-            <i class="fa fa-search"></i>
-        </button>
-        <button class="btn btn-default btn-xs public-key" key-body="{{ ",".join(key.pretty_permissions)|escape }}">
-            <i class="fa fa-key"></i>
-        </button>
-    {% if can_control %}
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/tag">
-            <span class="glyphicon glyphicon-tag" style="vertical-align: -1px"></span>
-        </a>
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/delete">
-            <span class="glyphicon glyphicon-remove" style="vertical-align: -1px"></span>
-        </a>
-        </span>
-    {% endif %}
-    </td>
-{%- endmacro %}
-
-{% macro public_key_panel(max_height, user, public_keys, can_control=False) -%}
-    {{ public_key_modal() }}
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Public Keys</h3>
-        </div>
-        <div class="table-responsive" style="max-height: {{max_height}}px; overflow-y: auto">
-            <table class="table table-striped table-condensed">
-                <thead>
-                    <tr>
-                        <th class="col-sm-1">Age</th>
-                        <th class="col-sm-1">Type</th>
-                        <th class="col-sm-1">Size</th>
-                        <th class="col-sm-3 hidden-xs">Fingerprint</th>
-                        <th class="col-sm-1">Tags</th>
-                        <th class="col-sm-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                {% for key in public_keys %}
-                    <tr>
-                        {{ one_public_key_row(key, user.name, can_control) }}
-                    </tr>
-                {% endfor %}
-                {% if not public_keys %}
-                    <tr>
-                        <td colspan="9" class="text-center"><em>No Public Keys</em></td>
-                    </tr>
-                {% endif %}
-                </tbody>
-            </table>
-            {% if can_control %}
-            <div class='panel-footer'>
-                <a class="btn btn-default btn-sm" href="/users/{{ user.name }}/public-key/add">
-                <span class="glyphicon glyphicon-plus"></span> Add Public Key
-                </a>
-            </div>
-            {% endif %}
-        </div>
-    </div>
-{%- endmacro %}
-
+{% from 'macros/ui.html' import public_key_modal with context %}
 
 {% block heading %}
     <a href="/service">Service Accounts</a>
@@ -98,7 +16,7 @@
     {% if not user.enabled %}<small>(disabled)</small>{% endif %}
 
     {% if group.audit and not group.audit.complete %}
-        <span class="label label-danger">AUDIT IN PROGRESS</i>
+        <span class="label label-danger">AUDIT IN PROGRESS</span>
     {% endif %}
 {% endblock %}
 
@@ -350,7 +268,7 @@
                     <tbody>
                     {% for member in group.audit.my_members() %}
                         <tr>
-                            <td>{{ account(member.member) }}</a></td>
+                            <td>{{ account(member.member) }}</td>
                             <td>{{ member.edge.role }}</td>
                             <td>
                                 {{ audit_status(member) }}

grouper/fe/templates/user.html
@@ -1,89 +1,8 @@
 {% extends "base.html" %}
-{% from 'macros/ui.html' import group_panel, permission_panel, log_entry_panel, tokens_panel, public_key_modal, shell_panel, passwords_panel %}
 
-{% macro one_public_key_row(key, username, can_control) -%}
-    <td><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
-        {{ key.created_on|long_ago_str }}</span></td>
-    <td>{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
-    <td>{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
-    <td class="hidden-xs">
-        <code>{{ key.fingerprint }}</code>
-    </td>
-    <td>
-        {% for tag in key.tags %}
-            {% if can_control %}
-            <form action="/users/{{ username }}/public-key/{{ key.id }}/delete_tag/{{ tag.id }}" method="post" style="display: inline">
-            {% endif %}
-                <button type="submit" class="btn btn-danger">
-                    {{ tag.name }}
-                </button>
-            {% if can_control %}
-                {{ xsrf_form() }}
-            </form>
-            {% endif %}
-        {% endfor %}
-    </td>
-    <td>
-        <span class="pull-right">
-        <button class="btn btn-default btn-xs public-key" key-body="{{ key.public_key|escape }}">
-            <i class="fa fa-search"></i>
-        </button>
-        <button class="btn btn-default btn-xs public-key" key-body="{{ ",".join(key.pretty_permissions)|escape }}">
-            <i class="fa fa-key"></i>
-        </button>
-    {% if can_control %}
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/tag">
-            <span class="glyphicon glyphicon-tag" style="vertical-align: -1px"></span>
-        </a>
-        <a class="btn btn-default btn-xs" href="/users/{{ username }}/public-key/{{ key.id }}/delete">
-            <span class="glyphicon glyphicon-remove" style="vertical-align: -1px"></span>
-        </a>
-        </span>
-    {% endif %}
-    </td>
-{%- endmacro %}
-
-{% macro public_key_panel(max_height, user, public_keys, can_control=False) -%}
-    {{ public_key_modal() }}
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Public Keys</h3>
-        </div>
-        <div class="table-responsive" style="max-height: {{max_height}}px; overflow-y: auto">
-            <table class="table table-striped table-condensed">
-                <thead>
-                    <tr>
-                        <th class="col-sm-1">Age</th>
-                        <th class="col-sm-1">Type</th>
-                        <th class="col-sm-1">Size</th>
-                        <th class="col-sm-3 hidden-xs">Fingerprint</th>
-                        <th class="col-sm-1">Tags</th>
-                        <th class="col-sm-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                {% for key in public_keys %}
-                    <tr>
-                        {{ one_public_key_row(key, user.name, can_control) }}
-                    </tr>
-                {% endfor %}
-                {% if not public_keys %}
-                    <tr>
-                        <td colspan="9" class="text-center"><em>No Public Keys</em></td>
-                    </tr>
-                {% endif %}
-                </tbody>
-            </table>
-            {% if can_control %}
-            <div class='panel-footer'>
-                <a class="btn btn-default btn-sm" href="/users/{{ user.name }}/public-key/add">
-                <span class="glyphicon glyphicon-plus"></span> Add Public Key
-                </a>
-            </div>
-            {% endif %}
-        </div>
-    </div>
-{%- endmacro %}
+{% from 'macros/ui.html' import group_panel, permission_panel, log_entry_panel, tokens_panel,
+                                shell_panel, passwords_panel %}
+{% from 'macros/ui.html' import public_key_panel with context %}
 
 {% block heading %}
     <a href="/users">Users</a>

commit 6d01992dac7797fd449c56666f9f2d2fcbdb9e1a

Fix /users/public-keys
This was broken in d005a71. Also backfill Selenium tests for this page.

grouper/fe/templates/macros/ui.html
@@ -1,9 +1,9 @@
 {% macro one_public_key_row(key, username, can_control) -%}
-    <td><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
+    <td class="public-key-created-on"><span class="has-help" data-toggle="popover" data-content="{{ key.created_on|print_date }}">
         {{ key.created_on|long_ago_str }}</span></td>
-    <td>{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
-    <td>{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
-    <td class="hidden-xs">
+    <td class="public-key-type">{% if key.key_type %}{{ key.key_type }}{% endif %}</td>
+    <td class="public-key-size">{% if key.key_size %}{{ key.key_size }}{% endif %}</td>
+    <td class="public-key-fingerprint-sha256 hidden-xs">
         <code>{{ key.fingerprint_sha256 }}</code>
     </td>
     <td>

grouper/fe/templates/users-publickey.html
@@ -1,8 +1,7 @@
 {% extends "base.html" %}
 
-{% from 'macros/ui.html' import account, paginator, one_public_key_row %}
-{% from 'macros/ui.html' import public_key_modal with context %}
-{% from 'macros/ui.html' import dropdown with context %}
+{% from 'macros/ui.html' import account, one_public_key_row %}
+{% from 'macros/ui.html' import dropdown, paginator, public_key_modal with context %}
 
 {% block heading %}
     {% if not form.enabled.data %}
@@ -68,8 +67,8 @@
             </thead>
             <tbody>
             {% for key, user in user_key_list %}
-                <tr>
-                    <td>{{ account(user) }}</td>
+                <tr class="public-key-row">
+                    <td class="public-key-user">{{ account(user) }}</td>
                     {{ one_public_key_row(key, user.name, True) }}
                 </tr>
             {% endfor %}

tests/pages.py
@@ -118,6 +118,18 @@ class AuditsCreatePage(BasePage):
         form.submit()
 
 
+class PublicKeysPage(BasePage):
+    def find_public_key_row(self, fingerprint_sha256):
+        for row in self.find_elements_by_class_name("public-key-row"):
+            member_row = PublicKeyRow(row)
+            if member_row.fingerprint_sha256 == fingerprint_sha256:
+                return member_row
+
+        raise NoSuchElementException(
+            "Can't find public key with fingerprint {}".format(fingerprint_sha256)
+        )
+
+
 class BaseModal(BaseElement):
     def confirm(self):
         form = self.find_element_by_tag_name("form")
@@ -191,3 +203,21 @@ class AuditMemberRow(BaseElement):
         status_cell = self.find_element_by_class_name("audit-member-status")
         status_select = status_cell.find_element_by_tag_name("select")
         Select(status_select).select_by_visible_text(status)
+
+
+class PublicKeyRow(BaseElement):
+    @property
+    def user(self):
+        return self.find_element_by_class_name("public-key-user").text
+
+    @property
+    def key_type(self):
+        return self.find_element_by_class_name("public-key-type").text
+
+    @property
+    def key_size(self):
+        return self.find_element_by_class_name("public-key-size").text
+
+    @property
+    def fingerprint_sha256(self):
+        return self.find_element_by_class_name("public-key-fingerprint-sha256").text

tests/test_fe_users.py
@@ -1,9 +1,17 @@
+import pytest
+
+from constants import SSH_KEY_1
 from fixtures import graph, groups, permissions, session, standard_graph, users  # noqa: F401
 from fixtures import fe_app as app  # noqa: F401
 from fixtures_selenium import async_server, browser  # noqa: F401
-from pages import UserViewPage
+from grouper.constants import AUDIT_SECURITY
+from grouper.models.permission import Permission
+from grouper.models.public_key import PublicKey
+from grouper.public_key import add_public_key
+from pages import PublicKeysPage, UserViewPage, NoSuchElementException
 from plugins import group_ownership_policy
 from url_util import url
+from util import add_member, grant_permission
 
 
 def test_disable_last_owner(async_server, browser):
@@ -19,3 +27,42 @@ def test_disable_last_owner(async_server, browser):
 
     assert page.current_url.endswith("/users/gary@a.co")
     assert page.has_text(group_ownership_policy.EXCEPTION_MESSAGE)
+
+
+def test_list_public_keys(async_server, browser, session, users, groups):
+    permission = Permission.get_or_create(session, name=AUDIT_SECURITY, description="")[0]
+    user = users["cbguder@a.co"]
+    group = groups["group-admins"]
+
+    add_member(group, user, role="owner")
+    grant_permission(group, permission, "public_keys")
+
+    # Pagination defaults to 100 keys per page
+    for i in range(120):
+        key = PublicKey(
+            user=user,
+            public_key="KEY:{}".format(i),
+            fingerprint="MD5:{}".format(i),
+            fingerprint_sha256="SHA256:{}".format(i),
+            key_size=4096,
+            key_type="ssh-rsa",
+            comment="",
+        )
+        key.add(session)
+
+    session.commit()
+
+    fe_url = url(async_server, "/users/public-keys")
+    browser.get(fe_url)
+
+    page = PublicKeysPage(browser)
+
+    row = page.find_public_key_row("SHA256:0")
+    assert row.user == user.username
+    assert row.key_size == "4096"
+    assert row.key_type == "ssh-rsa"
+
+    assert page.find_public_key_row("SHA256:99")
+
+    with pytest.raises(NoSuchElementException):
+        page.find_public_key_row("SHA256:100")
fee06f9154c69deea2eb688282f6edc6e9606bee
Can Berk Güder <cbguder@dropbox.com>
2018-04-17 16:09:46 -0700

    Use random ports for Selenium tests, override database URL via command line flags

--- grouper/fe/main.py
+++ grouper/fe/main.py
@@ -68,7 +68,8 @@ def start_server(args, sentry_client):
 
     # setup database
     logging.debug("configure database session")
-    Session.configure(bind=get_db_engine(get_database_url(settings)))
+    database_url = args.database_url or get_database_url(settings)
+    Session.configure(bind=get_db_engine(database_url))
 
     application = get_application(settings, sentry_client, args.deployment_name)
 

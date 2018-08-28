f84ea054d6dfda5623edeab155a35d06c2624113
Rick Mark <rickmark@dropbox.com>
2018-08-28 15:05:05 -0700

    Permission request UJS

--- grouper/fe/static/js/grouper.js
+++ grouper/fe/static/js/grouper.js
@@ -107,12 +107,83 @@ $(function () {
 
         var modal = $(e.currentTarget);
 
-        var form = modal.find(".revoke-permission-form")
+        var form = modal.find(".revoke-permission-form");
         form.attr("action", "/groups/{{group.name}}/service/{{user.username}}/revoke/" + mappingId);
     });
 
     var auditModal = $('#auditModal');
     if (auditModal.data('show') === true) {
         auditModal.modal('show');
+    }
+
+    var group_perm_request = $('#groupPermissionRequest');
+    if (group_perm_request.length)
+    {
+        var args_by_perm = group_perm_request.data('permission');
+
+        var $reason_divs = $('.form-group-reason');
+        var $argument_divs = $('.form-group-argument');
+
+        var $argument_select = $('#dropdown_form #argument');
+        var $argument_text = $('#text_form #argument');
+
+        var $dropdown_form_div = $('#dropdown_form');
+        var $text_form_div = $('#text_form');
+
+        var $perm_fields = $('.input-permission_name');
+        var $reason_fields = $('.input-reason');
+
+        function update_args() {
+            var args = args_by_perm[$perm_fields.val()];
+            if ($perm_fields.val() == "") {
+                // default we show dropdown form (we have to choose one) and
+                //    hide argument and reason fields (ux hint to select permission
+                //    first)
+                $argument_divs.hide();
+                $reason_divs.hide();
+
+                $dropdown_form_div.show();
+                $text_form_div.hide();
+            } else if (args.length == 1 && args[0] == "*") {
+                // change to text form field cause permission allows a wildcard argument
+                $argument_divs.show();
+                $reason_divs.show();
+
+                $dropdown_form_div.hide();
+                $text_form_div.show();
+
+                $argument_text.empty();
+            } else {
+                // change to dropdown form field cause permission only specific
+                //    specific arguments #}
+                $argument_divs.show();
+                $reason_divs.show();
+
+                $dropdown_form_div.show();
+                $text_form_div.hide();
+
+                $argument_select.empty();
+                $.each(args, function(index, arg) {
+                    var option = $("<option></option>").attr("value", arg).text(arg);
+                    $argument_select.append(option);
+                });
+            }
+        }
+
+        // prime it
+        update_args();
+
+        $perm_fields.change(function() {
+            // keep permission values synced
+            $perm_fields.val($(this).val());
+
+            // update view of the world
+            update_args();
+        });
+
+        $reason_fields.keyup(function() {
+            // keep reason values synced
+            $reason_fields.val($(this).val());
+        });
     }
 });

dba32c5c6f561ca00c9953297912b3f69f64b01d
Wesley Rodriguez <wrodriguez@dropbox.com>
2018-12-19 00:07:25 +0000

    Add permission requests page itests

---
+++ itests/pages/permission_requests.py
@@ -0,0 +1,55 @@
+from base import BaseElement, BasePage
+
+class PermissionRequestsPage(BasePage):
+    @property
+    def request_rows(self):
+        all_request_rows = self.find_elements_by_class_name('request-row')
+        return [RequestViewRow(row) for row in all_request_rows]
+
+    @property
+    def status_change_rows(self):
+        all_sc_rows = self.find_elements_by_class_name('request-status-change-row')
+        return [StatusChangeRow(row) for row in all_sc_rows]
+
+    @property
+    def no_requests_row(self):
+        return self.find_element_by_class_name('request-dne')
+
+
+class RequestViewRow(BaseElement):
+    @property
+    def modify_link(self):
+        modify = self.find_element_by_class_name('request-modify')
+        link = modify.find_element_by_tag_name('a')
+        return link.get_attribute('href')
+
+    @property
+    def requested(self):
+        return self.find_element_by_class_name('request-requested').text
+
+    @property
+    def approvers(self):
+        return self.find_element_by_class_name('request-approvers').text
+
+    @property
+    def status(self):
+        return self.find_element_by_class_name('request-status').text
+
+    @property
+    def requested_at(self):
+        return self.find_element_by_class_name('request-requested-at').text
+
+
+class StatusChangeRow(BaseElement):
+    @property
+    def group(self):
+        return self.find_element_by_class_name('request-group').text
+
+    @property
+    def who(self):
+        return self.find_element_by_class_name('request-who').text
+
+    @property
+    def reason(self):
+        return self.find_element_by_class_name('request-reason').text
+        

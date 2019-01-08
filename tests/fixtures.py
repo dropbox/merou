4796b5e8907eb9f760adab88f45b9efd4e5acefe
Giang Nguyen <cauthu@dropbox.com>
2019-01-08 20:05:48 +0000

    roll back unnecessary changes

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,4 @@
 import pytest
-from mock import patch
 
 from grouper.api.routes import HANDLERS as API_HANDLERS
 from grouper.app import Application
@@ -209,13 +208,7 @@ def permissions(session, users):
         for permission in all_permissions
     }
 
-    # enable_permission_auditing(session, permissions["audited"].name, users['zorkian@a.co'])
-    # during tests, by default, settings.auditors_group is None, so
-    # enable_permission_auditing() will complain.  this is the easy
-    # hack. alternately, we can make enable_permission_auditing() take
-    # a boolean `auto_promote_auditors` flag to disable the new code
-    permissions["audited"]._audited = True
-    session.commit()
+    enable_permission_auditing(session, permissions["audited"].name, users['zorkian@a.co'].id)
 
     return permissions
 

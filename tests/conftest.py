5b6e0b97393b6811a4a7d55379ea2d1cc75c7d09
Russ Allbery <rra@dropbox.com>
2019-02-14 15:59:42 -0800

    Refactor test setup for usecase tests
    Provide a new SetupTest class that handles creating the database
    session and provides utility functions for setting up test data.
    This is the replacement for fixtures that set up a complex data
    model that most tests don't need and that somewhat obscure what
    the test is doing.
    
    This is the first iteration, which messes with the database
    directly via SQLAlchemy objects.  A later revision will use the
    same service and repository path that non-test code uses.
    
    This is implemented as a fixture rather than an explicit call
    because, when testing with a persistant database, we have to close
    the session or the next test will hang while trying to drop the
    schema to ensure fresh test data due to the leaked previous session.
    Having to remember to close the session is irritating, as is having
    to write every test inside a with clause.  A pytest fixture is
    simplest for the test writer.
    
    To avoid endless flake8 warnings, provide the fixture via
    conftest.py so that pytest will inject it into every test without
    an explicit import.  (This is somewhat spooky action at a distance,
    but it saves a lot of repetitive typing.)

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+"""Provide pytest fixtures for test setup.
+
+When testing with a persistant database, we have to explicitly close the database session after
+each test.  Otherwise, the presence of an open session will prevent dropping all tables to ensure a
+clean test context.  The easiest way to provide that is via pytest fixtures.
+
+This file is automatically loaded by pytest and injects available fixtures into every test without
+requiring the flake8 noqa annotations normally needed by explicit fixture imports.
+"""
+
+from contextlib import closing
+from typing import TYPE_CHECKING
+
+import pytest
+
+from tests.setup import SetupTest
+
+if TYPE_CHECKING:
+    from py import LocalPath
+    from typing import Iterator
+
+
+@pytest.yield_fixture
+def setup(tmpdir):
+    # type: (LocalPath) -> Iterator[SetupTest]
+    with closing(SetupTest(tmpdir)) as test_setup:
+        yield test_setup
---
+++ tests/setup.py
@@ -0,0 +1,149 @@
+"""Utilities to set up test cases.
+
+Provides a SetupTest object that creates the database session, provides use case factories for
+individual tests, and provides methods to create objects in the test database.  These methods try
+to minimize the amount of code required to set up a test by creating new objects whenever needed.
+So, for instance, one can just call:
+
+    setup.add_user_to_group("user@a.co", "some-group")
+
+without creating the user and group first, and both will be created if not present.
+
+This is the new test setup mechanism, replacing the fixtures defined in tests.fixtures.  All new
+tests should use this mechanism and not rely on standard_graph or other pytest fixtures.
+"""
+
+import os
+from datetime import datetime
+from time import time
+from typing import TYPE_CHECKING
+
+from grouper.graph import GroupGraph
+from grouper.models.base.constants import OBJ_TYPES
+from grouper.models.base.model_base import Model
+from grouper.models.base.session import get_db_engine, Session
+from grouper.models.counter import Counter
+from grouper.models.group import Group
+from grouper.models.group_edge import GROUP_EDGE_ROLES, GroupEdge
+from grouper.models.permission import Permission
+from grouper.models.permission_map import PermissionMap
+from grouper.models.user import User
+from grouper.repositories.factory import RepositoryFactory
+from grouper.services.factory import ServiceFactory
+from grouper.usecases.factory import UseCaseFactory
+from tests.path_util import db_url
+
+if TYPE_CHECKING:
+    from py.local import LocalPath
+    from typing import Optional
+
+
+class SetupTest(object):
+    """Set up the environment for a test.
+
+    Attributes:
+        graph: Underlying graph (not refreshed from the database automatically!)
+        repository_factory: Factory for repository objects
+        session: The underlying database session
+        service_factory: Factory for service objects
+        usecase_factory: Factory for usecase objects
+    """
+
+    def __init__(self, tmpdir):
+        # type: (LocalPath) -> None
+        self.session = self.create_session(tmpdir)
+        self.graph = GroupGraph()
+        self.repository_factory = RepositoryFactory(self.session, self.graph)
+        self.service_factory = ServiceFactory(self.session, self.repository_factory)
+        self.usecase_factory = UseCaseFactory(self.service_factory)
+
+    def create_session(self, tmpdir):
+        # type: (LocalPath) -> Session
+        db_engine = get_db_engine(db_url(tmpdir))
+
+        # If using a persistant database, clear the database first.
+        if "MEROU_TEST_DATABASE" in os.environ:
+            Model.metadata.drop_all(db_engine)
+
+        # Create the database schema and the corresponding session.
+        Model.metadata.create_all(db_engine)
+        Session.configure(bind=db_engine)
+        return Session()
+
+    def commit(self):
+        # type: () -> None
+        Counter.incr(self.session, "updates")
+        self.session.commit()
+        self.graph.update_from_db(self.session)
+
+    def close(self):
+        # type: () -> None
+        self.session.close()
+
+    def create_group(self, name):
+        # type: (str) -> None
+        """Create a group, does nothing if it already exists."""
+        if Group.get(self.session, name=name):
+            return
+        group = Group(groupname=name)
+        group.add(self.session)
+
+    def create_permission(
+        self, name, description="", audited=False, enabled=True, created_on=None
+    ):
+        # type: (str, str, bool, bool, Optional[datetime]) -> None
+        """Create a permission, does nothing if it already exists.
+
+        Avoid milliseconds in the creation timestamp since they behave differently in SQLite (which
+        preserves them) and MySQL (which drops them).
+        """
+        if Permission.get(self.session, name=name):
+            return
+        if not created_on:
+            created_on = datetime.utcfromtimestamp(int(time()))
+        permission = Permission(
+            name=name,
+            description=description,
+            _audited=audited,
+            enabled=enabled,
+            created_on=created_on,
+        )
+        permission.add(self.session)
+
+    def create_user(self, name):
+        # type: (str) -> None
+        """Create a user, does nothing if it already exists."""
+        if User.get(self.session, name=name):
+            return
+        user = User(username=name)
+        user.add(self.session)
+
+    def add_user_to_group(self, user, group, role="member"):
+        # type: (str, str, str) -> None
+        self.create_user(user)
+        self.create_group(group)
+        user_obj = User.get(self.session, name=user)
+        assert user_obj
+        group_obj = Group.get(self.session, name=group)
+        assert group_obj
+        edge = GroupEdge(
+            group_id=group_obj.id,
+            member_type=OBJ_TYPES["User"],
+            member_pk=user_obj.id,
+            active=True,
+            _role=GROUP_EDGE_ROLES.index(role),
+        )
+        edge.add(self.session)
+
+    def grant_permission_to_group(self, group, permission, argument):
+        # type: (str, str, str) -> None
+        self.create_group(group)
+        self.create_permission(permission)
+        permission_obj = Permission.get(self.session, name=permission)
+        assert permission_obj
+        group_obj = Group.get(self.session, name=group)
+        assert group_obj
+        grant = PermissionMap(
+            permission_id=permission_obj.id, group_id=group_obj.id, argument=argument
+        )
+        grant.add(self.session)

8d92d1c8d4f30ae2ddcf8dbb6b2884d9956b859c
Benjamin Peterson <benjamin@python.org>
2019-02-22 15:22:55 -0800

    Support ed25519 public keys.
    ed25519 keys get exempt from size checks; all well-formed keys should be sufficiently large.
    
    For variety, I modified one fe integration test to use the ed25519 fixture key instead of a rsa one.

--- tests/ssh_key_policy_test.py
+++ tests/ssh_key_policy_test.py
@@ -4,17 +4,24 @@ from mock import patch
 from grouper.plugin.proxy import PluginProxy
 from grouper.public_key import add_public_key, BadPublicKey
 from plugins.ssh_key_policy import SshKeyPolicyPlugin
-from tests.constants import SSH_KEY_1, SSH_KEY_DSA, SSH_KEY_ECDSA_P256, SSH_KEY_RSA_1024
+from tests.constants import (
+    SSH_KEY_1,
+    SSH_KEY_DSA,
+    SSH_KEY_ECDSA_P256,
+    SSH_KEY_ED25519,
+    SSH_KEY_RSA_1024,
+)
 from tests.fixtures import session, users  # noqa: F401
 
 
 @patch("grouper.public_key.get_plugin_proxy")
-def test_accepts_strong_rsa_keys(get_plugin_proxy, session, users):  # noqa: F811
+def test_accepts_strong_keys(get_plugin_proxy, session, users):  # noqa: F811
     get_plugin_proxy.return_value = PluginProxy([SshKeyPolicyPlugin()])
 
     user = users["cbguder@a.co"]
 
     add_public_key(session, user, SSH_KEY_1)
+    add_public_key(session, user, SSH_KEY_ED25519)
 
 
 @patch("grouper.public_key.get_plugin_proxy")

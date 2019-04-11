edb18a1731d22f667b919cec76523c07837616f4
Russ Allbery <rra@dropbox.com>
2019-04-10 22:18:57 -0700

    Add docstring for permission_aliases.py plugin
    What this plugin does is a little opaque, so add a docstring
    explaining what's going on.

--- plugins/permission_aliases.py
+++ plugins/permission_aliases.py
@@ -1,3 +1,10 @@
+"""Sample plugin that defines a permission alias.
+
+This plugin makes the "owner" permission equivalent to having the "ssh" permission with argument
+"owner=<argument>" and the "sudo" permission with argument <argument>, where <argument> is the
+argument to the "owner" permission.
+"""
+
 from grouper.plugin.base import BasePlugin
 
 

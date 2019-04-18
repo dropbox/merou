835a5b3f80ebc67191d08c735f83c6cab13ae725
Russ Allbery <rra@dropbox.com>
2019-04-18 16:19:17 -0700

    Only support datetime in template formatting
    The print_date, expires_when_str, and long_ago_str template utility
    functions supported formatting datetime, seconds since epoch, and
    a specific string value for a datetime, which added a fair bit of
    complexity.  Before refactoring HTML template handling for better
    web security, clean this up, since it's not necessary.
    
    We were passing dates other than datetime into these functions for
    only two reasons:
    
    * Group.my_members() was doing tricky SQLAlchemy work to merge users
      and groups, which for reasons I don't understand was coercing the
      expiration to a string.  Use much simpler logic to avoid this, at
      the minor cost of one additional round trip to the database.
    * Permission information was taken from the graph, which uses seconds
      since epoch for time values.  This is easy to convert to datetime
      before doing template formatting.
    
    Add explicit formatting to email audit templates instead of using the
    default formatting for Jinja2 (which doesn't honor Grouper settings),
    caught by review of this diff.
    
    Remove some old templates that are no longer referenced.

--- tests/fe/util_test.py
+++ tests/fe/util_test.py
@@ -12,35 +12,47 @@ def test_expires_when_str():
 
     assert expires_when_str(None) == "Never", "no datetime means no expires"
 
-    for date_, expected, msg in [
-        ("2015-08-11 11:00:00.000", "Expired", "long before should expire"),
-        ("2015-08-11 12:00:00.000", "Expired", "same time should expire"),
-        ("2015-08-11 11:59:59.000", "Expired", "minute after should expire"),
-        ("2015-08-11 12:00:00.100", "Expired", "milliseonds before should expire"),
-        ("2015-08-11 12:00:01.000", "1 second", "singular second"),
-        ("2015-08-11 12:00:02.000", "2 seconds", "pural second"),
-        ("2015-08-11 12:01:02.000", "1 minute", "ignore lower periods"),
-        ("2016-08-11 12:01:02.000", "1 year", "ignore lower periods"),
-        (datetime(2015, 8, 11, 12, 0, 1, 0, tzinfo=UTC), "1 second", "from datetime object"),
-        (1439294401.0, "1 second", "from float / unix timestamp"),
+    for date, expected, msg in [
+        (datetime(2015, 8, 11, 11, 00, 00, 0, tzinfo=UTC), "Expired", "long before should expire"),
+        (datetime(2015, 8, 11, 12, 00, 00, 0, tzinfo=UTC), "Expired", "same time should expire"),
+        (
+            datetime(2015, 8, 11, 11, 59, 59, 0, tzinfo=UTC),
+            "Expired",
+            "minute after should expire",
+        ),
+        (
+            datetime(2015, 8, 11, 12, 0, 0, 100, tzinfo=UTC),
+            "Expired",
+            "milliseonds before should expire",
+        ),
+        (datetime(2015, 8, 11, 12, 0, 1, 0, tzinfo=UTC), "1 second", "singular second"),
+        (datetime(2015, 8, 11, 12, 0, 2, 0, tzinfo=UTC), "2 seconds", "pural second"),
+        (datetime(2015, 8, 11, 12, 1, 2, 0, tzinfo=UTC), "1 minute", "ignore lower periods"),
+        (datetime(2016, 8, 11, 12, 1, 2, 0, tzinfo=UTC), "1 year", "ignore lower periods"),
     ]:
-        assert expires_when_str(date_, utcnow_fn=utcnow_fn) == expected, msg
+        assert expires_when_str(date, utcnow_fn=utcnow_fn) == expected, msg
 
 
 def test_long_ago_str():
     utcnow_fn = utcnow_fn = lambda: datetime(2015, 8, 11, 12, tzinfo=UTC)
 
-    for date_, expected, msg in [
-        ("2015-08-11 11:00:00.000", "1 hour ago", "long before should expire"),
-        ("2015-08-11 12:00:00.000", "now", "now"),
-        ("2015-08-11 11:59:59.100", "now", "milliseonds before should be now"),
-        ("2015-08-11 11:59:00.000", "1 minute ago", "1 minute"),
-        ("2015-08-11 11:58:00.000", "2 minutes ago", "pural minutes"),
-        ("2015-08-11 12:00:01.000", "in the future", "in the future"),
-        (datetime(2015, 8, 11, 11, 0, 0, 0, tzinfo=UTC), "1 hour ago", "from datetime object"),
-        (1439290800.0, "1 hour ago", "from float / unix timestamp"),
+    for date, expected, msg in [
+        (
+            datetime(2015, 8, 11, 11, 0, 0, 0, tzinfo=UTC),
+            "1 hour ago",
+            "long before should expire",
+        ),
+        (datetime(2015, 8, 11, 12, 0, 0, 0, tzinfo=UTC), "now", "now"),
+        (
+            datetime(2015, 8, 11, 11, 59, 59, 100, tzinfo=UTC),
+            "now",
+            "milliseconds before should be now",
+        ),
+        (datetime(2015, 8, 11, 11, 59, 0, 0, tzinfo=UTC), "1 minute ago", "1 minute"),
+        (datetime(2015, 8, 11, 11, 58, 0, 0, tzinfo=UTC), "2 minutes ago", "pural minutes"),
+        (datetime(2015, 8, 11, 12, 0, 1, 0, tzinfo=UTC), "in the future", "in the future"),
     ]:
-        assert long_ago_str(date_, utcnow_fn=utcnow_fn) == expected, msg
+        assert long_ago_str(date, utcnow_fn=utcnow_fn) == expected, msg
 
 
 def test_print_date():
@@ -54,7 +66,5 @@ def test_print_date():
     for date_, expected, msg in [
         (datetime(2015, 8, 11, 18, tzinfo=UTC), "2015-08-11 11:00 AM", "from datetime object"),
         (datetime(2015, 8, 11, 18, 0, 10, 10, tzinfo=UTC), "2015-08-11 11:00 AM", "ignore sec/ms"),
-        ("2015-08-11 18:00:00.000", "2015-08-11 11:00 AM", "from string"),
-        (1439316000.0, "2015-08-11 11:00 AM", "from float / unix timestamp"),
     ]:
         assert print_date(date_) == expected, msg

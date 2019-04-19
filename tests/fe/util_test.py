1e7fd877023a4f415a586c39cc392c55d2aae98d
Russ Allbery <rra@dropbox.com>
2019-04-19 16:00:53 -0700

    Rewrite date tests to be easier to read
    Test both with and without a timezone set to make sure that we
    add a timezone where necessary (and that also makes the tests more
    readable).  Be clearer about how we're testing with milliseconds.

--- tests/fe/util_test.py
+++ tests/fe/util_test.py
@@ -7,52 +7,41 @@ from grouper.fe.template_util import expires_when_str, long_ago_str, print_date
 from grouper.settings import set_global_settings, Settings
 
 
-def test_expires_when_str():
-    utcnow_fn = utcnow_fn = lambda: datetime(2015, 8, 11, 12, tzinfo=UTC)
+def mock_utcnow():
+    # type: () -> datetime
+    return datetime(2015, 8, 11, 12, tzinfo=UTC)
 
+
+def test_expires_when_str():
+    # type: () -> None
     assert expires_when_str(None) == "Never", "no datetime means no expires"
 
     for date, expected, msg in [
-        (datetime(2015, 8, 11, 11, 00, 00, 0, tzinfo=UTC), "Expired", "long before should expire"),
-        (datetime(2015, 8, 11, 12, 00, 00, 0, tzinfo=UTC), "Expired", "same time should expire"),
-        (
-            datetime(2015, 8, 11, 11, 59, 59, 0, tzinfo=UTC),
-            "Expired",
-            "minute after should expire",
-        ),
-        (
-            datetime(2015, 8, 11, 12, 0, 0, 100, tzinfo=UTC),
-            "Expired",
-            "milliseonds before should expire",
-        ),
-        (datetime(2015, 8, 11, 12, 0, 1, 0, tzinfo=UTC), "1 second", "singular second"),
-        (datetime(2015, 8, 11, 12, 0, 2, 0, tzinfo=UTC), "2 seconds", "pural second"),
-        (datetime(2015, 8, 11, 12, 1, 2, 0, tzinfo=UTC), "1 minute", "ignore lower periods"),
-        (datetime(2016, 8, 11, 12, 1, 2, 0, tzinfo=UTC), "1 year", "ignore lower periods"),
+        (datetime(2015, 8, 11, 11, 00, 00, 0), "Expired", "long before should expire"),
+        (datetime(2015, 8, 11, 12, 00, 00, 0), "Expired", "same time should expire"),
+        (datetime(2015, 8, 11, 11, 59, 59, 0), "Expired", "minute after should expire"),
+        (datetime(2015, 8, 11, 12, 0, 0, 100), "Expired", "milliseonds should be ignored"),
+        (datetime(2015, 8, 11, 12, 0, 1, 0), "1 second", "singular second"),
+        (datetime(2015, 8, 11, 12, 0, 2, 0), "2 seconds", "pural second"),
+        (datetime(2015, 8, 11, 12, 1, 2, 0), "1 minute", "ignore lower periods"),
+        (datetime(2016, 8, 11, 12, 1, 2, 0), "1 year", "ignore lower periods"),
     ]:
-        assert expires_when_str(date, utcnow_fn=utcnow_fn) == expected, msg
+        assert expires_when_str(date.replace(tzinfo=UTC), utcnow_fn=mock_utcnow) == expected, msg
+        assert expires_when_str(date, utcnow_fn=mock_utcnow) == expected, msg + " (no tzinfo)"
 
 
 def test_long_ago_str():
-    utcnow_fn = utcnow_fn = lambda: datetime(2015, 8, 11, 12, tzinfo=UTC)
-
+    # type: () -> None
     for date, expected, msg in [
-        (
-            datetime(2015, 8, 11, 11, 0, 0, 0, tzinfo=UTC),
-            "1 hour ago",
-            "long before should expire",
-        ),
-        (datetime(2015, 8, 11, 12, 0, 0, 0, tzinfo=UTC), "now", "now"),
-        (
-            datetime(2015, 8, 11, 11, 59, 59, 100, tzinfo=UTC),
-            "now",
-            "milliseconds before should be now",
-        ),
-        (datetime(2015, 8, 11, 11, 59, 0, 0, tzinfo=UTC), "1 minute ago", "1 minute"),
-        (datetime(2015, 8, 11, 11, 58, 0, 0, tzinfo=UTC), "2 minutes ago", "pural minutes"),
-        (datetime(2015, 8, 11, 12, 0, 1, 0, tzinfo=UTC), "in the future", "in the future"),
+        (datetime(2015, 8, 11, 11, 0, 0, 0), "1 hour ago", "long before should expire"),
+        (datetime(2015, 8, 11, 12, 0, 0, 0), "now", "now"),
+        (datetime(2015, 8, 11, 11, 59, 59, 100), "now", "milliseconds should be ignored"),
+        (datetime(2015, 8, 11, 11, 59, 0, 0), "1 minute ago", "1 minute"),
+        (datetime(2015, 8, 11, 11, 58, 0, 0), "2 minutes ago", "pural minutes"),
+        (datetime(2015, 8, 11, 12, 0, 1, 0), "in the future", "in the future"),
     ]:
-        assert long_ago_str(date, utcnow_fn=utcnow_fn) == expected, msg
+        assert long_ago_str(date.replace(tzinfo=UTC), utcnow_fn=mock_utcnow) == expected, msg
+        assert long_ago_str(date, utcnow_fn=mock_utcnow) == expected, msg + " (no tzinfo)"
 
 
 def test_print_date():
@@ -66,5 +55,6 @@ def test_print_date():
     for date_, expected, msg in [
         (datetime(2015, 8, 11, 18, tzinfo=UTC), "2015-08-11 11:00 AM", "from datetime object"),
         (datetime(2015, 8, 11, 18, 0, 10, 10, tzinfo=UTC), "2015-08-11 11:00 AM", "ignore sec/ms"),
+        (datetime(2015, 8, 11, 18, 0, 10, 10), "2015-08-11 11:00 AM", "add tzinfo if needed"),
     ]:
         assert print_date(date_) == expected, msg

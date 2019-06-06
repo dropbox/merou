bda7cb008df4a99fc95a6f89cdabffd5329e5e8f
Russ Allbery <rra@dropbox.com>
2019-06-06 10:17:32 -0700

    Remove SQL implementation of all_enabled_users
    This isn't needed yet and probably will never be needed, since this
    specific function is likely only useful for the API server, which
    can answer it from the graph.

--- grouper/repositories/user.py
+++ grouper/repositories/user.py
@@ -1,16 +1,13 @@
-from collections import defaultdict
 from typing import TYPE_CHECKING
 
-from grouper.entities.user import PublicKey, User, UserMetadata, UserNotFoundException
-from grouper.models.public_key import PublicKey as SQLPublicKey
+from grouper.entities.user import User, UserNotFoundException
 from grouper.models.user import User as SQLUser
-from grouper.models.user_metadata import UserMetadata as SQLUserMetadata
 from grouper.repositories.interfaces import UserRepository
 
 if TYPE_CHECKING:
     from grouper.graph import GroupGraph
     from grouper.models.base.session import Session
-    from typing import Dict, List
+    from typing import Dict
 
 
 class GraphUserRepository(UserRepository):
@@ -52,35 +49,7 @@ class SQLUserRepository(UserRepository):
 
     def all_enabled_users(self):
         # type: () -> Dict[str, User]
-        metadata = defaultdict(list)  # type: Dict[int, List[UserMetadata]]
-        for user_metadata in self.session.query(SQLUserMetadata):
-            metadata[user_metadata.user_id].append(
-                UserMetadata(key=user_metadata.data_key, value=user_metadata.data_value)
-            )
-
-        public_keys = defaultdict(list)  # type: Dict[int, List[PublicKey]]
-        for public_key in self.session.query(SQLPublicKey):
-            public_keys[public_key.user_id].append(
-                PublicKey(
-                    public_key=public_key.public_key,
-                    fingerprint=public_key.fingerprint,
-                    fingerprint_sha256=public_key.fingerprint_sha256,
-                )
-            )
-
-        users = {}  # type: Dict[str, User]
-        sql_users = self.session.query(SQLUser).filter(
-            SQLUser.enabled == True, SQLUser.is_service_account == False
-        )
-        for user in sql_users:
-            users[user.username] = User(
-                name=user.username,
-                enabled=user.enabled,
-                role_user=user.role_user,
-                metadata=metadata.get(user.id, []),
-                public_keys=public_keys.get(user.id, []),
-            )
-        return users
+        raise NotImplementedError()
 
     def disable_user(self, name):
         # type: (str) -> None

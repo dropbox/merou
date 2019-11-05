7be7e10ea11f6554ceb984471f87c42d2eebbfd5
Russ Allbery <rra@dropbox.com>
2019-11-05 12:22:26 -0800

    Strip argument in permission request, add end-to-end test
    The new permission request page uses Tornado's get_argument(), which
    strips whitespace by default, but it's not obvious that it always
    goes through that code path.  Explicitly strip the argument and add an
    end-to-end test of the permission request and approval flow that
    confirms that leading and trailing whitespace in the permission grant
    argument is stripped but internal whitespace is preserved.

--- itests/pages/permission_requests.py
+++ itests/pages/permission_requests.py
@@ -1,55 +1,87 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from selenium.webdriver.support.select import Select
+
 from itests.pages.base import BaseElement, BasePage
+
+if TYPE_CHECKING:
+    from selenium.webdriver.remote.webelement import WebElement
+    from typing import List
 
 
 class PermissionRequestsPage(BasePage):
     @property
-    def request_rows(self):
+    def request_rows(self) -> List[RequestViewRow]:
         all_request_rows = self.find_elements_by_class_name("request-row")
         return [RequestViewRow(row) for row in all_request_rows]
 
     @property
-    def status_change_rows(self):
+    def status_change_rows(self) -> List[StatusChangeRow]:
         all_sc_rows = self.find_elements_by_class_name("request-status-change-row")
         return [StatusChangeRow(row) for row in all_sc_rows]
 
     @property
-    def no_requests_row(self):
+    def no_requests_row(self) -> WebElement:
         return self.find_element_by_class_name("request-dne")
+
+
+class PermissionRequestUpdatePage(BasePage):
+    @property
+    def form(self) -> WebElement:
+        return self.find_element_by_id("update-form")
+
+    def set_status(self, status: str) -> None:
+        field = Select(self.form.find_element_by_name("status"))
+        field.select_by_value(status)
+
+    def set_reason(self, reason: str) -> None:
+        field = self.form.find_element_by_name("reason")
+        field.send_keys(reason)
+
+    def submit(self) -> None:
+        self.form.submit()
 
 
 class RequestViewRow(BaseElement):
     @property
-    def modify_link(self):
+    def modify_link(self) -> str:
         modify = self.find_element_by_class_name("request-modify")
         link = modify.find_element_by_tag_name("a")
         return link.get_attribute("href")
 
     @property
-    def requested(self):
+    def requested(self) -> str:
         return self.find_element_by_class_name("request-requested").text
 
     @property
-    def approvers(self):
+    def approvers(self) -> str:
         return self.find_element_by_class_name("request-approvers").text
 
     @property
-    def status(self):
+    def status(self) -> str:
         return self.find_element_by_class_name("request-status").text
 
     @property
-    def requested_at(self):
+    def requested_at(self) -> str:
         return self.find_element_by_class_name("request-requested-at").text
+
+    def click_modify_link(self) -> None:
+        modify = self.find_element_by_class_name("request-modify")
+        link = modify.find_element_by_tag_name("a")
+        link.click()
 
 
 class StatusChangeRow(BaseElement):
     @property
-    def group(self):
+    def group(self) -> str:
         return self.find_element_by_class_name("request-group").text
 
     @property
-    def who(self):
+    def who(self) -> str:
         return self.find_element_by_class_name("request-who").text
 
     @property
-    def reason(self):
+    def reason(self) -> str:
         return self.find_element_by_class_name("request-reason").text

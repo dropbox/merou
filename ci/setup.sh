78f47c8ca850888c4b21022476eff17b5badc9a6
akorzo <akorzo@dropbox.com>
2023-03-13 09:33:00 -0700

    attempt to configure/create merou databse from github-actions config

--- ci/setup.sh
+++ ci/setup.sh
@@ -7,9 +7,5 @@ echo "y" | unzip chromedriver_linux64.zip -d chromedriver
 
 echo "Creating Database: $1"
 
-if [ "$1" = 'mysql' ]; then
-    mysql -e 'CREATE DATABASE merou CHARACTER SET utf8mb4;'
-fi
-
 pip3 install -r requirements.txt
 pip3 install -r requirements-dev.txt
